fa5075047cb7ab14e1211cf3e5d729e32dcbf47a
Larissa Teles <adv.larissateles@gmail.com>
2026-07-29 17:25:17 -0300

    Corrige a criação do cliente Supabase para usar a variável supabaseClient

--- supabase_confg.js
+++ supabase_confg.js
@@ -1,4 +1,4 @@
 const supabase_url = 'https://droabquslsnmgiazqqsk.supabase.co' //url do banco 
 const supabase_key = 'sb_publishable_csZKUtyP88vQeg4wV7Lxsw_D9rACesy' //chave pública
 
-const supabase = createClient(supabase_url, supabase_key);
+const supabaseClient = supabase.createClient(supabase_url, supabase_key);
